420f3a4115096e4bbb9788c4fcff8473f20280d9
Shane Guittap <shaneguittap@Shanes-MacBook-Pro.local>
2019-10-18 13:32:18 -0400

    items creation and prisma yoga workflow

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,9 +1,12 @@
 const Mutations = {
-    createDog(parent, args, ctx, info){
-        global.dogs = global.dogs || [];
-        const newDog = {name:args.name};
-        global.dogs.push(newDog);
-        return newDog;
+    async createItem(parent, args, ctx, info){
+        //todo: checked if they are logged in
+        const item =  await ctx.db.mutation.createItem({
+            data:{
+                ...args
+            }
+        },info);
+        return item;
     }
 };
 
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,9 +1,8 @@
 const Query = {
-    dogs(parent, args, ctx, info){
-        global.dogs = global.dogs || [];
-        return global.dogs;
+    async items(parent, args, ctx, info){
+        const items = await ctx.db.query.items()
+        return items
     }
-
 };
 
 module.exports = Query;
