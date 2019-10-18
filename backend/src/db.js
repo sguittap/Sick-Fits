d6321b9083dcf736612eb158bc274f7586634840
Shane Guittap <shaneguittap@Shanes-MacBook-Pro.local>
2019-10-18 11:35:09 -0400

    graphql yoga server running

---
+++ backend/src/db.js
@@ -0,0 +1,12 @@
+//this file connects to the remote prisma db and allows us to query db with javascript
+
+const { Prisma } = require('prisma-binding');
+
+const db = new Prisma({
+    typeDefs: 'src/generated/prisma.graphql',
+    endpoint: process.env.PRISMA_ENDPOINT,
+    secret: process.env.PRISMA_SECRET,
+    debug: false,
+});
+
+module.exports = db
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,14 @@
-// let's go!
+require('dotenv').config({ path:'variables.env'});
+const createServer = require ('./createServer');
+const db = require ('./db');
+
+const server =  createServer();
+
+server.start({
+    cors: {
+        credentials: true,
+        origin: process.env.FRONTEND_URL,
+    }
+}, deets => {
+    console.log(`Server running on http:/localhost:${deets.port}`)
+});
