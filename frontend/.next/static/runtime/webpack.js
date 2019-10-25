9e368dc0b8e24439b19ab0a64aeb96fd633eed69
Shane Guittap <shaneguittap@Shanes-MacBook-Pro.local>
2019-10-25 18:13:34 -0400

    updating items with queries and mutations

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "28c647ae937c944aa283";
+/******/ 	var hotCurrentHash = "2f4f74aa111e794617ef";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
